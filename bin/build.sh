66ab8400f86470c5810a874f9382259b304a3d59
shun <shun@getto.systems>
2017-04-13 15:47:53 +0900

    remove dist from source

--- bin/build.sh
+++ bin/build.sh
@@ -5,14 +5,15 @@ build_main(){
   local file
 
   version=$(grep '"version":' package.json | cut -d'"' -f4)
+  rm -rf public/dist
+
+  for file in public/*.html; do
+    sed -i "s/\/dist\//\/$version\//g" $file
+  done
 
   if [ "$(curl -sI -o /dev/null -w "%{http_code}" $AWS_URL/$version/getto.css)" == "404" ]; then
     npm run build
     mv public/dist public/$version
-
-    for file in public/*.html; do
-      sed -i "s/\/dist\//\/$version\//g" $file
-    done
   fi
 }
 
