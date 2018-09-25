87d972277e445e1bba0e321af3a5847e8b8a9913
shun <shun@getto.systems>
2018-09-26 06:13:28 +0900

    fix: redirect

--- public/dist/js/redirect.js
+++ public/dist/js/redirect.js
@@ -7,19 +7,36 @@ var check_versions = function(current) {
 
   var versions = [];
 
-  var next = function(v, index) {
-    v = v.slice();
-    if (v.length > index) {
-      v[index] = parseInt(v[index]) + 1;
+  var next_patch = function(v) {
+    v = v.slice(0.3);
+    if (v.length > 2) {
+      v[2] = parseInt(v[2]) + 1;
     }
+    return v.join(".");
+  };
+  var next_minor = function(v) {
+    v = v.slice(0,2);
+    if (v.length > 1) {
+      v[1] = parseInt(v[1]) + 1;
+    }
+    v.push(0);
+    return v.join(".");
+  };
+  var next_major = function(v) {
+    v = v.slice(0,1);
+    if (v.length > 0) {
+      v[0] = parseInt(v[0]) + 1;
+    }
+    v.push(0);
+    v.push(0);
     return v.join(".");
   };
 
   var info = current.split(".");
 
-  info.forEach(function(_, i) {
-    versions.push(next(info, i));
-  });
+  versions.push(next_patch(info, i));
+  versions.push(next_minor(info, i));
+  versions.push(next_major(info, i));
 
   return versions;
 }
