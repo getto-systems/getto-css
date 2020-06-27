c26d47b077a2370910d76de3ee54b45d653edb0c
shun <shun.fix9@gmail.com>
2020-06-27 12:51:49 +0000

    fix: CSP

--- metadata/css.js
+++ metadata/css.js
@@ -15,13 +15,12 @@ const headers = {
     ].join(" "),
     "style-src " + [
       "'self'",
-      "'filesystem:'",
-      "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
-      "'sha256-2A5Wtu5POsS6Hj3CQs8vKYei6omWRq7MzlUZF7VwmHs='",
     ].join(" "),
     "style-src-elem " + [
       "'self'",
       "https://trellis.getto.systems/css",
+      "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+      "'sha256-2A5Wtu5POsS6Hj3CQs8vKYei6omWRq7MzlUZF7VwmHs='",
     ].join(" "),
   ].join(";"),
   "x-content-type-options": "nosniff",
