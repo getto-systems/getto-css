85ad4697deee3636133e4c99f479b5d65d2a9902
shun <shun.fix9@gmail.com>
2021-01-10 07:15:21 +0000

    remove: unused

--- theme/lib/auth/common/loginID/data.ts
+++
@@ -1,4 +0,0 @@
-export type LoginID = string & { LoginID: never }
-export function markLoginID(loginID: string): LoginID {
-    return loginID as LoginID
-}
--- theme/lib/auth/common/password/data.ts
+++
@@ -1,4 +0,0 @@
-export type Password = string & { Password: never }
-export function markPassword(password: string): Password {
-    return password as Password
-}
