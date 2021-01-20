206d1e9bd14bae1a23b8a2ee5cb68816b6939dfe
shun <shun.fix9@gmail.com>
2021-01-20 10:28:06 +0000

    fix: global info

--- theme/lib/x_preact/Outline/Menu/MainMenu.ts
+++ theme/lib/x_preact/Outline/Menu/MainMenu.ts
@@ -5,10 +5,15 @@ import { appMenu, menuBox, menuFooter, menuHeader } from "../../common/layout"
 import { MenuList } from "../MenuList"
 
 import { MenuListComponent } from "../../../auth/Outline/menuList/component"
+import { form } from "../../Document/box"
 
 type Props = Readonly<{
     menuList: MenuListComponent
 }>
 export function MainMenu(props: Props): VNode {
-    return appMenu([menuHeader(), menuBox("global information"), h(MenuList, props), menuFooter()])
+    return appMenu([menuHeader(), menuBox(globalInformation()), h(MenuList, props), menuFooter()])
+}
+
+function globalInformation(): VNode {
+    return form("information", "global state")
 }
