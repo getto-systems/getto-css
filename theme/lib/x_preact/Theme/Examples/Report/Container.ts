38070c37da95ff3a420a1cc7e71c11a7de7de529
shun <shun.fix9@gmail.com>
2021-01-31 17:33:52 +0000

    fix: report example

--- theme/lib/x_preact/Theme/Examples/Report/Container.ts
+++ theme/lib/x_preact/Theme/Examples/Report/Container.ts
@@ -1,8 +1,6 @@
 import { h, VNode } from "preact"
-import { useMemo, useState } from "preact/hooks"
-import { html } from "htm/preact"
+import { useMemo } from "preact/hooks"
 
-import { SearchForm } from "./SearchForm"
 import { buildStructure, Content } from "./Content"
 import { generateRows, Model, Row } from "./data"
 import { visibleAll } from "../../../../z_external/getto-table/preact/core"
@@ -35,34 +33,5 @@ export function Container(_: ContainerProps): VNode {
         column: (row: Row) => structure.column(params, row),
     }
 
-    return html` ${h(SearchForm, useSearchProps())} ${h(Content, contentProps)} `
-
-    function useSearchProps(): DocumentProps {
-        const [state, setState] = useState<DocumentState>(initialDocument)
-        const component: DocumentComponent = {
-            search: () => {
-                setState({ type: "try-to-search" })
-
-                setTimeout(() => {
-                    setState(initialDocument)
-                }, 3000)
-            },
-        }
-        return { state, component }
-    }
-}
-
-export type DocumentProps = Readonly<{
-    state: DocumentState
-    component: DocumentComponent
-}>
-export interface DocumentComponent {
-    search: Post<null>
-}
-export type DocumentState = Readonly<{ type: "search" }> | Readonly<{ type: "try-to-search" }>
-
-const initialDocument: DocumentState = { type: "search" }
-
-interface Post<T> {
-    (event: T): void
+    return h(Content, contentProps)
 }
