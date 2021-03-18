12cdf12eb1c7e5bf9ae26808cf4be30a78d7b2a2
shun <shun.fix9@gmail.com>
2021-03-18 19:23:16 +0000

    fix: docs

--- theme/lib/x_main/docs/index/foreground.ts
+++ theme/lib/x_main/docs/index/foreground.ts
@@ -3,9 +3,13 @@ import { render, h } from "preact"
 
 import { newDocsView } from "../../../docs/action_docs/init"
 
-import { docs_example } from "../../../theme/docs"
-import { docs_avail } from "../../../avail/docs"
-import { docs_docs } from "../../../docs/docs"
+import {
+    docs_theme,
+    docs_theme_base,
+    docs_theme_design,
+    docs_theme_details,
+    docs_theme_layout,
+} from "../../../theme/docs"
 
 import { DocsEntry } from "../../../docs/action_docs/x_preact/docs"
 
@@ -13,7 +17,12 @@ render(
     h(
         DocsEntry({
             title: "ドキュメント",
-            contents: [[docs_example], [[...docs_docs, ...docs_avail]]],
+            contents: [
+                [docs_theme],
+                [[...docs_theme_base, ...docs_theme_layout]],
+                [docs_theme_design],
+                [docs_theme_details],
+            ],
         }),
         newDocsView({
             webStorage: localStorage,
